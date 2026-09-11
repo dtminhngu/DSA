82ff2484862c7071d12a23fb31a5cc8b55b37ebd
Minh <duongtuanminh07@gmail.com>
2026-09-11 15:04:21 +0700

    them note vao file template fixed sized window

--- Sliding_Window/Fixed_sized_window_template.cpp
+++ Sliding_Window/Fixed_sized_window_template.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 int Max_Sum_SubArray (vector <int> &arr, int k) {
+    //1. Create the first window with the size of k, calculate the sum of first k elements
     int current_sum = 0;
     int max_sum = 0;
     for (int i=0 ;i<k; i++) current_sum += arr[i];
     max_sum = current_sum;
-
+    //2. Slide the window from index k to the end of the array
     for (int i=k; i<arr.size(); i++) {
-        current_sum = current_sum + arr[i] - arr[i-k];
-        max_sum = max (max_sum, current_sum);
+        current_sum = current_sum + arr[i] - arr[i-k]; // update current_sum
+        max_sum = max (max_sum, current_sum); //compare max_sum and current_sum to get maximum result
     }
     return max_sum;
 }
